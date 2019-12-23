6c8a3a60a2df6e193d041395c46e11cdb2241d07
JAVA <JAVA@WS-246-03.avalon.ru>
2019-12-23 20:50:15 +0300

    Вторая личинка, исправленная и дополненная

--- src/ru/avalon/java/j30/labs/script.sql
+++ src/ru/avalon/java/j30/labs/script.sql
@@ -1,3 +1,4 @@
+--create schema marat;-- identified by qwerty
 /*
  * TODO(Студент): Опишите процесс создания базы данных
  * 1. Создайте все таблицы, согласно предложенной диаграмме.
@@ -5,75 +6,93 @@
  * 3. Добавьте в каждую таблицу минимум три записи.
  */
 
-create table order(
-  id      integer(10),
-  user    integer(10),
-  created timestamp default systimestamp,
+create table orders(
+  id       integer,
+  "user"   integer,
+  created  timestamp,-- default systimestamp,
   constraint order_pk primary key(id)
 );
 
-insert into order(id, user) values (1, 1);
-insert into order(id, user) values (2, 1);
-insert into order(id, user) values (3, 2);
+insert into orders(id, "user", created) values (1, 1, '2019-12-23 12:12:12');
+insert into orders(id, "user", created) values (2, 1, '2019-12-23 15:34:34.003434');
+--insert into "order"(id, "user") values (3, 2);
 
 
-create table user(
-  id       integer(10) generated always as identity,
+create table users(
+  id       integer generated always as identity,
   email    varchar(255),
-  password varchar(255) not null,
-  info     integer(10),
-  role     integer(10),
+  password varchar(255),
+  info     integer,
+  role     integer,
   constraint email_pk primary key(email),
-  constraint info_uk unique (info)
+  constraint users_info_uk unique (info),
+  constraint users_id_uk unique (id)
 );
+
+insert into users(email, info, role) values ('user1@mail.com',  1, 1);
+--insert into users(id, email, info, role) values (1, "user2@mail.com", 2, 1);
+--insert into users(id, email, info, role) values (1, "admin@mail.com", 3, 2);
+
 create table userinfo(
-  id       integer(10),
+  id       integer,
   name     varchar(255) not null,
   surname  varchar(255) not null,
   constraint userinfo_pk primary key (id)
 );
 
+insert into userinfo(id, name, surname) values (1, 'Вася', 'Пупкин');
+insert into userinfo(id, name, surname) values (2, 'Иван', 'ургант');
+
 create table roles(
-  id    integer(10) generated always as identity,
+  id    integer generated always as identity,
   name  varchar(255),
   constraint roles_pk primary key(name),
-  constraint info_uk unique (id)
+  constraint info_id_uk unique (id)
 );
+
+insert into roles (name) values ('User');
+insert into roles (name) values ('Admin');
 
 create table product(
-  id            integer(10) generated always as identity,
+  id            integer generated always as identity,
   code          varchar(255),
-  title         varchar(255), --not null
-  supplier      integer(10),
-  initial_price double(10),
-  retail_Value  double(10),
+  title         varchar(255),
+  supplier      integer,
+  initial_price double,
+  retail_Value  double,
   constraint product_pk primary key(code),
   constraint product_uk unique (id)
-
 );
 
+insert into product ( code, title, supplier, initial_price) values ('product 1', 'title 1', 1, 100);
+insert into product ( code, title, supplier, initial_price) values ('product 2', 'title 2', 1, 200);
+
 create table supplier (
-  id             integer(10) generated always as identity,
+  id             integer generated always as identity,
   name           varchar(255),
   address        varchar(255) not null,
   phone          varchar(255),
   representative varchar(255) not null,
   constraint supplier_pk primary key (name),
   constraint supplier_uk unique (id)
-
 );
+
+insert into supplier( name, address, phone, representative) values ('Supplier1', 'Address 1', '1234567', '????');
 
 create table order2product(
-  order integer(10) not null,
-  product integer(10) not null
+  "order" integer not null,
+  product integer not null
 );
 
+insert into order2product ("order", product) values (1, 1);
 
-alter table order add constraint order_info_fk foreign key(user) references user(id);
-alter table user add constraint user_info_fk foreign key(info) references userinfo(id);
-alter table user add constraint user_role_fk foreign key(role) references roles(id);
 
-alter table order2product add constraint o2p_order_fk foreign key(order) references order(id);
+
+alter table orders add constraint order_info_fk foreign key("user") references users(id);
+alter table users add constraint user_info_fk foreign key(info) references userinfo(id);
+alter table users add constraint user_role_fk foreign key(role) references roles(id);
+
+alter table order2product add constraint o2p_order_fk foreign key("order") references orders(id);
 alter table order2product add constraint o2p_product_fk foreign key(product) references product(id);
 
 alter table product add constraint prod_supplier_fk foreign key(supplier) references supplier(id);
